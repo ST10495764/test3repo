1816ce7d0ca4daf1fb887c8f4d73e48cf320f7f3
ST10495764 <ST10495764@VCCONNECT.EDU.ZA>
2026-03-12 17:08:09 +0200

    updated migration

---
+++ EventEase/Migrations/20260312134113_nullable.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventEase.Migrations
+{
+    /// <inheritdoc />
+    public partial class nullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Event_EventId",
+                table: "Booking");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Venue_VenueId",
+                table: "Booking");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VenueName",
+                table: "Venue",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VenueLocation",
+                table: "Venue",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VenueImg",
+                table: "Venue",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VenueCapaity",
+                table: "Venue",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EventImg",
+                table: "Event",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VenueId",
+                table: "Booking",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventId",
+                table: "Booking",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Event_EventId",
+                table: "Booking",
+                column: "EventId",
+                principalTable: "Event",
+                principalColumn: "EventId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Venue_VenueId",
+                table: "Booking",
+                column: "VenueId",
+                principalTable: "Venue",
+                principalColumn: "VenueId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Event_EventId",
+                table: "Booking");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Venue_VenueId",
+                table: "Booking");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VenueName",
+                table: "Venue",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VenueLocation",
+                table: "Venue",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VenueImg",
+                table: "Venue",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VenueCapaity",
+                table: "Venue",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EventImg",
+                table: "Event",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VenueId",
+                table: "Booking",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventId",
+                table: "Booking",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Event_EventId",
+                table: "Booking",
+                column: "EventId",
+                principalTable: "Event",
+                principalColumn: "EventId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Venue_VenueId",
+                table: "Booking",
+                column: "VenueId",
+                principalTable: "Venue",
+                principalColumn: "VenueId");
+        }
+    }
+}
