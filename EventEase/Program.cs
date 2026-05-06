8cfc70b7fbce50153f92e86cfac9428d1bbfad50
ST10495764 <ST10495764@VCCONNECT.EDU.ZA>
2026-05-06 22:06:14 +0200

    REGISTERING BLOB SERVICE

--- EventEase/Program.cs
+++ EventEase/Program.cs
@@ -19,6 +19,8 @@ namespace EventEase
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddScoped<BlobService>();//REGISTERING BLOB SERVICE
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
